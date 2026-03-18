7c14a4056f5728439c30a6624e31bd32cdcb2a00
MUKILTEO\1717487 <Fgreenmun@gmail.com>
2026-03-18 13:28:52 -0700

    A

---
+++ Assets/Finn/Old Project/Enemy.cs
@@ -0,0 +1,175 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+using Unity.VisualScripting;
+
+
+[System.Serializable]
+public class AnimationBlend
+{
+    #region Variables
+    [SerializeField] Enemy enemy;
+
+    [Space(10)]
+
+    public Animator animator;
+
+    [Space(10)]
+
+    float blend;
+    [Tooltip("Animtion blend speed")] [Range(0, 1)] [SerializeField] float smoothing = 0.5f;
+    #endregion
+    #region Methods
+    public void Update()
+    {
+        bool moving = enemy.agent.velocity.magnitude > 0.5f;
+        blend = Mathf.Lerp(blend, moving ? 1 : 0, smoothing);
+
+        animator.SetFloat("Movement", blend);
+    }
+    #endregion
+}
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy")]
+
+    public NavMeshAgent agent;
+    float initialSpeed;
+
+    [Range(0, 1)] public float health = 1;
+
+    bool died;
+
+    Transform plr;
+    character_movement plrHealth;
+
+    [Header("Attack")]
+
+    [Range(0, 1)] [SerializeField] float damage = 0.125f;
+
+    [Space(10)]
+
+    public float attackRange = 1f;
+    float range;
+    bool inRange;
+
+    [Tooltip("Time enemy is in range before affecting player")] [SerializeField] float attackTime = 0.15f;
+    [Tooltip("Attack cooldown (Recommened to use animation length)")] [SerializeField] float attackSleepTime = 0.25f;
+    bool attackDeb;
+
+    [Space(10)]
+
+    [SerializeField] bool explodeOnDeath;
+    [Range(0, 1)] [SerializeField] float maxExplodeDamge;
+    public float minExplodeRadius;
+    public float maxExplodeRadius;
+
+
+    [Header("Animation")]
+
+    [SerializeField] AnimationBlend movementBlend;
+
+    [Header("More")]
+
+    [SerializeField] GameObject ghost;
+
+
+
+    void Awake()
+    {
+        initialSpeed = agent.speed;
+
+        plr = GameObject.Find("PLAYER").transform;
+        plrHealth = plr.GetComponent<character_movement>();
+    }
+    
+
+    void Update()
+    {
+        if ( died == true )
+        {
+
+            DeathDrops();
+           
+        }
+        health = Mathf.Clamp01(health);
+
+        range = (transform.position - plr.position).magnitude;
+        inRange = (range < attackRange);
+
+        //Follow
+
+        agent.SetDestination(plr.position);
+        agent.isStopped = inRange;
+
+        if (health > 0)
+            agent.speed = (!attackDeb) ? initialSpeed : initialSpeed / 2;
+
+
+        //Attack
+
+        if (inRange && !attackDeb && !died)
+        {
+            attackDeb = true;
+
+            movementBlend.animator.Play("Attack", 1);
+
+           
+        }
+
+        //death
+        if (health <= 0 && !died)
+        {
+            died = true;
+           
+
+            ghost.transform.SetParent(null); //unparent
+            ghost.SetActive(true);
+
+            agent.speed = 0;
+            movementBlend.animator.Play("Death");
+
+          
+
+            if (!explodeOnDeath) return;
+
+            float distance = (transform.position - plr.transform.position).magnitude;
+            distance = (distance < 0.01f) ? 0 : distance;
+
+            //damage based on distance
+           
+        }
+
+        #region ANIMATION
+        movementBlend.Update();
+        #endregion
+    }
+    public void DeathDrops()
+    {
+        Debug.Log("0");
+        GetComponent<materialDrop>().triggered = true;
+        
+           
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(Enemy))]
+class EnemyEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        Enemy enemy = (Enemy)target;
+
+        Handles.color = Color.red;
+        Handles.DrawWireDisc(enemy.transform.position, Vector3.up, enemy.attackRange);
+
+        Handles.color = Color.red / 2;
+        Handles.DrawWireDisc(enemy.transform.position, Vector3.up, enemy.maxExplodeRadius);
+        Handles.DrawWireDisc(enemy.transform.position, Vector3.up, enemy.minExplodeRadius);
+    }
+}
+#endif
