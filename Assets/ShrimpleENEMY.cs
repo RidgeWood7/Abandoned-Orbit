ff91be41beacb6eb2423b42cd45d5324af74f78c
MUKILTEO\1717487 <Fgreenmun@gmail.com>
2026-03-30 11:16:44 -0700

    a

--- Assets/ShrimpleENEMY.cs
+++ Assets/ShrimpleENEMY.cs
@@ -1,18 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShrimpleENEMY : MonoBehaviour
 {
     public Transform Playerpos;
+    public Slider HealthBar;
+    public float MaxEHealth;
+    public float EHealth;
     UnityEngine.AI.NavMeshAgent agent;
+    public bool isDead = false;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        EHealth = MaxEHealth;
     }
     void Update()
     {
-
+        HealthBar.value = EHealth;
+        HealthBar.maxValue = MaxEHealth;
         agent.destination = Playerpos.position;
+        if (EHealth < 0  )
+        {
+            isDead = true;
+            Destroy(gameObject);
+
+        }
+
 
     }
 }
