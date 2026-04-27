71973f5e27bd42590e240ed1157dd179a20ae2e6
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2026-04-27 12:57:41 -0700

    Reapply "Fixing My Audio Manager so it actually will play the audio properly with scaling"
    This reverts commit ba72e0a6cb3b1ed45a774c87e84632be7923dd88.

---
+++ Assets/EthanJKarban/Scripts/AuDM.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class AuDM : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
+
+    public AudioClip background;
+    public AudioClip death;
+    public AudioClip fire;
+    public AudioClip walk;
+    public AudioClip kill;
+    public AudioClip doorOpened;
+    public AudioClip Button;
+    public AudioClip Charge;
+    public AudioClip HealAndPowerUp;
+
+}
